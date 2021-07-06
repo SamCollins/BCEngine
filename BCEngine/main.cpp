be32af96064008b9a7a90541891244a23c41de2f
SamCollins <samcollins427@gmail.com>
2021-07-05 21:56:20 -0700

    Added test rect

--- BCEngine/main.cpp
+++ BCEngine/main.cpp
@@ -7,11 +7,22 @@ int main(int argc, char *argv[])
 {
 	SDL_Init(SDL_INIT_EVERYTHING);
 	SDL_Window* window = SDL_CreateWindow("Test Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-		600, 400, SDL_WINDOW_SHOWN);
+		600, 600, SDL_WINDOW_SHOWN);
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
 
 	SDL_SetRenderDrawColor(renderer, 0, 255, 255, 255);
 	SDL_RenderClear(renderer);
+	SDL_RenderPresent(renderer);
+
+	SDL_Rect testRect;
+
+	testRect.x = 200;
+	testRect.y = 200;
+	testRect.w = 200;
+	testRect.h = 200;
+
+	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+	SDL_RenderFillRect(renderer, &testRect);
 	SDL_RenderPresent(renderer);
 
 	bool gameRunning = true;
